9f823cf9ec46314276ff40220ffd226482ba1e54
shadowcat0202 <fxlimsin@gmail.com>
2022-04-20 15:30:24 +0900

    new feat cpp

---
+++ CPP/Solution/Solution/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <filesystem>
+#include "test.h"
+#pragma warning(disable:4996)
+void init() {
+	std::ios_base::sync_with_stdio(0);
+	std::cin.tie(0);
+	freopen("./input.txt", "r", stdin);
+}
+void stub() {
+	test t;
+	t.sayhi();
+
+		
+}
+int main() {
+	stub();
+	
+	return 0;
+}
---
+++ CPP/Solution/Solution/test.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include "test.h"
+void test::sayhi() {
+	printf("hello");
+}
